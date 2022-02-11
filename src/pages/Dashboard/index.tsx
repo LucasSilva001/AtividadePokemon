28c56e40be9c2153954d0030634d1d6def52025e
LucasSilva001 <lucasdaraelu@gmail.com>
2022-02-11 16:17:41 -0300

    Pesquisa de CEP

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,75 @@
-import React from "react";
-import { Container, Title, Form } from "./styles";
+import React, { useState, FormEvent } from "react";
+import api from '../../services/api';
+
+import { Container, Title, Form, Ceps } from "./styles";
+
+interface CepProps {
+  cep: string;
+  logradouro: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+}
 
 const Dashboard: React.FC = () => {
+  const [newCep, setNewCep] = useState('');
+
+
+  const pesquisarCEP = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try{
+      const response = await api.get(`${newCep}/json/`);
+      const cepDados = response.data;
+
+      console.log(cepDados);
+  } catch(err) {
+
+  }
+};
+
   return (
     <Container>
       <Title>Pesquise endereços por CEP</Title>
 
-      <Form>
-        <input type="text" placeholder="Digite o CEP" />
+      <Form onSubmit={pesquisarCEP}>
+        <input 
+          type="form" 
+          placeholder="Digite o CEP" 
+          onChange={e => setNewCep(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
+      <Ceps>
+        <a href="#">
+          <p className="uf">SC</p>
+          <div>
+            <strong>Jaraguá do Sul</strong>
+            <p>Rua: Isidoro Pedri</p>
+            <p>Bairro: Barra do Rio Molha</p>
+            <p>CEP: 89253-387</p>
+          </div>
+        </a>
+
+        <a href="#">
+          <p className="uf">SC</p>
+          <div>
+            <strong>Jaraguá do Sul</strong>
+            <p>Rua: Isidoro Pedri</p>
+            <p>Bairro: Barra do Rio Molha</p>
+            <p>CEP: 89253-387</p>
+          </div>
+        </a>
+        <a href="#">
+          <p className="uf">SC</p>
+          <div>
+            <strong>Jaraguá do Sul</strong>
+            <p>Rua: Isidoro Pedri</p>
+            <p>Bairro: Barra do Rio Molha</p>
+            <p>CEP: 89253-387</p>
+          </div>
+        </a>
+      </Ceps>
     </Container>
   );
 };
